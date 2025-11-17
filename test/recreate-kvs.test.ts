101e1c09e15eb7bca56526e79ca41a2b34ebceb0
Ichiro Hiraide <hiraide@atama.plus>
2025-11-17 09:26:13 +0900

    cdk init

---
+++ test/recreate-kvs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RecreateKvs from '../lib/recreate-kvs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/recreate-kvs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RecreateKvs.RecreateKvsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
